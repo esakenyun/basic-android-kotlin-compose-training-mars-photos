b3c418666482cea4a7ed02fcfaa2b3281a6dcf5b
esakenyun <rfahlevih@gmail.com>
2024-12-18 23:09:13 +0700

    Update Lanjutan 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,11 +83,12 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
     // Kotlin serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     // Retrofit with Kotlin serialization Converter
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
-
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
